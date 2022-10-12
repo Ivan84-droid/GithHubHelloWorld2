b8b6af20f6041be3464f237935dd20615c4c7f29
IvanVojtek <vojteki@gmail.com>
2022-10-12 18:22:55 +0200

    New GitHub Hello World project with message Hello Wolrd in GIT Edit message Hello Wolrd in GIT 2. Edit message Hello Wolrd in GIT 3. Edit message Hello Wolrd in GIT 4.

--- src/GitHubHelloWorld.java
+++ src/GitHubHelloWorld.java
@@ -3,5 +3,6 @@ public class GitHubHelloWorld {
         System.out.println("Hello World in GIT");
         System.out.println("Helo World in GIT 2");
         System.out.println("Hello World in GIT 3");
+        System.out.println("Hello World in GIT 4");
     }
 }
